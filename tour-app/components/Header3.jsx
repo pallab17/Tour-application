3faa8482b7c4a7b0184d5475cec2f9c69dbdb21b
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-11-08 17:44:19 +0530

    commit

--- tour-app/components/Header3.jsx
+++ tour-app/components/Header3.jsx
@@ -26,7 +26,28 @@ const Header3 = () => {
             placeholder="Search..."
             className=" h-16 outline-none px-3 text-lg col-span-1"
           />
+          <button
+            type="submit"
+            className=" h-16 px-3 py-2 w-72 bg-green-500 hover:cursor-pointer hover:bg-green-600 text-white text-xl"
+          >
+            {/* <Link href={`/hotels?city=${city}`}>Search</Link> */}
+            Search
+          </button>
       </div>
+      <div className="flex mx-20 my-5 font-bold">
+          <button
+            type="submit"
+            className=" h-16 px-3 py-2 hover:cursor-pointer text-white mr-5"
+          >
+            Continue your search
+          </button>
+          <button
+            type="submit"
+            className=" h-16 px-3 py-2 hover:cursor-pointer border-2 border-white text-white mr-5 hover:bg-gray-500 rounded-xl"
+          >
+            Homestay in India hotels
+          </button>
+        </div>
     </div>
     </div>
   )

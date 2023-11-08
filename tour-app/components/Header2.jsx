0099d25ffaa6fc975bc16af0e7ac8e9972e8002e
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-11-08 17:02:19 +0530

    commit

---
+++ tour-app/components/Header2.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+const Header2 = () => {
+    const List = [
+        {
+          name: "Banglore",
+        },
+        {
+          name: "Culcutta",
+        },
+        {
+          name: "Mumbai",
+        },
+        {
+          name: "Delhi",
+        },
+        {
+          name: "Hyderabad",
+        },
+      ];
+  return (
+    <div>
+    
+      
+    </div>
+  )
+}
+
+export default Header2

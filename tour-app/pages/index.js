18e94ba2b4edfded3d66ecf31155580635ca3e89
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-11-09 22:28:32 +0530

    commit

--- tour-app/pages/index.js
+++ tour-app/pages/index.js
@@ -2,13 +2,41 @@ import React from 'react'
 import Header1 from '@/components/Header1'
 import Header2 from '@/components/Header2'
 import Header3 from '@/components/Header3'
+import Image from "next/image";
+import Head from 'next/head';
 
 const Home = () => {
   return (
     <div>
+    <Head>
+    <title>
+          OYO : India`s Best Online Hotel Booking Site For Sanitized Stay.
+        </title>
+        <link rel='icon' href='/icon.png'></link>
+        </Head>
      <Header1/>
      <Header2/>
      <Header3/>
+     <div className="mx-20">
+        <div className=" my-14">
+          <Image
+            src={"/banner1.avif"}
+            alt="banner1"
+            width={200}
+            height={200}
+            className=" h-80 w-full"
+          />
+        </div>
+        <div className="mb-14">
+          <Image
+            src={"/banner2.avif"}
+            alt="banner1"
+            width={200}
+            height={200}
+            className=" h-40 w-full"
+          />
+        </div>
+        </div>
     </div>
   )
 }

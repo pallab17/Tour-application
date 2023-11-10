c488f56d2abcba2306d249d71836a7a08e86435c
pallab17 <91554568+pallab17@users.noreply.github.com>
2023-11-11 01:58:47 +0530

    commit

--- tour-app/components/Header3.jsx
+++ tour-app/components/Header3.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
+"use client";
+
+import Link from "next/link";
+import { useState } from "react";
 
 const Header3 = () => {
+  const [city, setCity] = useState(" ");
+
   return (
-    <div className=' bg-gradient-to-r from-red-600 to-red-500 h-60 '>
+    <div className=" bg-gradient-to-r from-red-600 to-red-400 h-60">
       <div className=" p-5">
-      <h2 className=" text-4xl text-white text-center font-bold">
+        <h2 className=" text-4xl text-white text-center font-bold">
           Over 157,000 hotels and homes across 35 countries
         </h2>
         <div className="flex justify-center my-5 mx-20 ">
@@ -12,11 +17,11 @@ const Header3 = () => {
             type="text"
             placeholder="Search..."
             className=" w-6/12  h-16 outline-none px-3 text-lg border-r-2 border-gray-400 "
-            // onChange={(e) => {
-            //   setCity(e.target.value);
-            // }}
+            onChange={(e) => {
+              setCity(e.target.value);
+            }}
           />
-           <input
+          <input
             type="date"
             placeholder="Search..."
             className="  h-16 outline-none px-3 text-lg border-r-2 border-gray-400 col-span-1"
@@ -26,18 +31,18 @@ const Header3 = () => {
             placeholder="Search..."
             className=" h-16 outline-none px-3 text-lg col-span-1"
           />
+
           <button
             type="submit"
-            className=" h-16 px-3 py-2 w-72 bg-green-500 hover:cursor-pointer hover:bg-green-600 text-white text-xl"
+            className=" h-16 px-3 py-2 w-72 bg-green-400 hover:cursor-pointer hover:bg-green-600 text-white text-xl"
           >
-            {/* <Link href={`/hotels?city=${city}`}>Search</Link> */}
-            Search
+            <Link href={`/hotels?city=${city}`}>Search</Link>
           </button>
-      </div>
-      <div className="flex mx-20 my-5 font-bold">
+        </div>
+        <div className="flex mx-20 my-5 font-bold">
           <button
             type="submit"
-            className="  h-16 px-3 py-2 hover:cursor-pointer text-white mr-5"
+            className=" h-16 px-3 py-2 hover:cursor-pointer text-white mr-5"
           >
             Continue your search
           </button>
@@ -48,9 +53,9 @@ const Header3 = () => {
             Homestay in India hotels
           </button>
         </div>
+      </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
 export default Header3;
